d964f193b235e2452457c98c7b2aeb209e84b5ae
Elly Chan <elly.chan.ec@gmail.com>
2019-06-10 00:54:23 +0800

    first commit

---
+++ list.py
@@ -0,0 +1,43 @@
+# data types (int float bool str list)
+
+a = ['Toyota', 'Honda']
+print(a)
+print(a[0])
+print(a[1])
+
+a.append('Audi')
+print(a)
+
+a.append(123)
+print(a)
+
+print(len(a))
+
+print('Audi' in a)
+print('Benz' in a)
+
+if ('Audi' in a) == True:
+	print(123)
+
+cars = ['Toyota', 'Honda']
+
+for car in cars:
+	print(car)
+
+names = ['Allen', 'Tom', 'Jack']
+for name in names:
+	print(name)
+
+car = 'Audi'
+
+for c in car:
+	print(c)
+
+print(len(car))
+
+print('A' in car)
+
+print('Au' in car)
+
+print('x' in car)
+
